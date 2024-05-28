97fceec1b7503ec0f4362e0d36ae98c30778daa4
s25917 <s25917@pjwstk.edu.pl>
2024-05-28 16:27:11 +0200

    Bruh

---
+++ WebApplication1/WebApplication1/Controllers/TripsControllers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers;
+[ApiController]
+[Route("api/{controllers}")]
+public class TripsControllers: ControllerBase
+{
+    private readonly BruhContext _context;
+
+    public TripsControllers(BruhContext bruhContext)
+    {
+        _context = bruhContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var trips = await _context.Trips.ToListAsync();
+        
+        return Ok();
+    }
+}
